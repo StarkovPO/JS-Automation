86e9a63c004107f40a448e06867777fca4c6ff99
Seva Ostakhnovich <seva_ost@mail.ru>
2023-02-21 21:05:40 +0400

    adding new favorites test

--- tests/test/playwright/favorites.add-remove.test.js
+++ tests/test/playwright/favorites.add-remove.test.js
@@ -3,73 +3,92 @@ import { Homepage } from './pageobject/page.home';
 import { LoginPage } from './pageobject/page.login';
 import { Catalog } from './pageobject/page.catalog';
 import { Favorites } from './pageobject/page.favorites';
+import { Header } from './pageobject/page.header';
 
-//The tests shoul be launched one by one
-test.describe('Testing Favorites functionality: hearts and specific section', async () => {
+//The tests should be launched one by one
+test.describe.only('Testing Favorites functionality: hearts and specific section', async () => {
 
-test('Check Favourites heart-icon in Catalog', async ({page}) => {
-//test('Check Favourites icons', async ({browser}) => {     #authorization via auth.json file
-  // const context = await browser.newContext({             #should be updated from time to time
-  //   storageState: "./playwright-auth.json"
-  // })
-  // //const page = await context.newPage();
+  test.beforeEach(async ({ page }) => {
 
-  const homepage = new Homepage(page);
-  const loginpage = new LoginPage(page);
-  const catalog = new Catalog(page);
- 
-  await homepage.goto();
-  await page.waitForTimeout(2000);
-  await homepage.passCookies();
-  await homepage.provideDeliveryAddress('Hans-Sachs-Straße 4/b-c, Berlin, 12205');
-  await page.waitForTimeout(3000);
-
-  await homepage.gotoLogin2();
-  await page.waitForTimeout(2000);
-
-  //await loginpage.passCookies();
-  //await page.waitForTimeout(1000);
-  await loginpage.passAuth();
-  await page.waitForTimeout(2000);
-
-  await homepage.gotoMarket();
-  await page.waitForTimeout(2000);
-
-  await catalog.clickHeart();
-  await expect(catalog.heartWrapper).toHaveClass(/favorite/);
-});
-
-test('Check Favourites heart-icon in specific section', async ({page}) => {
-  //test('Check Favourites icons', async ({browser}) => {     #authorization via auth.json file
-    // const context = await browser.newContext({             #should be updated from time to time
-    //   storageState: "./auth.json"
-    // })
-    // //const page = await context.newPage();
-  
     const homepage = new Homepage(page);
     const loginpage = new LoginPage(page);
-    const favorites = new Favorites(page);
-     
-    await homepage.goto();
+    await homepage.gotoProduction();
     await homepage.passCookies();
     await homepage.provideDeliveryAddress('Hans-Sachs-Straße 4/b-c, Berlin, 12205');
-    await page.waitForTimeout(3000);
-  
-    await homepage.gotoLogin2();
-    await page.waitForTimeout(2000);
-  
-    //await loginpage.passCookies();
-    //await page.waitForTimeout(1000);
+    await homepage.clickOnAnmeldenButton();
     await loginpage.passAuth();
-    await page.waitForTimeout(2000);
-  
-    await homepage.gotoFavorites();
-    await page.waitForTimeout(2000);
-  
-    await favorites.clickHeart();
-    await page.waitForTimeout(2000);
 
-    await expect(favorites.allFavorites).toBeEmpty();
+    // const [response] = await Promise.all([
+    //   page.waitForRequest('**/login_url'),
+    //   page.waitForResponse('**/login_url'),
+    //   loginpage.passAuth()
+    // ]);
+    // expect(response.ok()).toBeTruthy();
   });
+
+test('Check Favourites heart-icon in Catalog', async ({page}) => {
+  
+  const homepage = new Homepage(page);
+  const catalog = new Catalog(page);
+  const favorites = new Favorites(page);
+  const header = new Header(page);
+  await page.waitForLoadState('networkidle');
+  await homepage.goMarketButton.click();
+  await catalog.firstHeart.click();
+  await header.profileDropButton.click();
+  await header.favoriteButton.click();
+  // await page.waitForLoadState('networkidle');
+  // const locator = favorites.zumProduktButton
+  // await expect(locator).toBeVisible();
+});
+
+test.afterEach(async ({ page }) => {
+
+    const homepage = new Homepage(page);
+    const loginpage = new LoginPage(page);
+    const header = new Header(page);
+    const favorites = new Favorites(page);
+    await homepage.gotoProduction();
+    await homepage.passCookies();
+    await homepage.provideDeliveryAddress('Hans-Sachs-Straße 4/b-c, Berlin, 12205');
+    await homepage.clickOnAnmeldenButton();
+    await loginpage.passAuth();
+    await header.profileDropButton.click();
+    await header.favoriteButton.click();
+    await page.waitForLoadState('networkidle');
+    await Promise.all([
+      page.waitForRequest('**/toggle.json'),
+      favorites.favoriteButton.click()
+    ])
+
+})
+
+// test('Check Favourites heart-icon in specific section', async ({page}) => {
+  
+//     const homepage = new Homepage(page);
+//     const loginpage = new LoginPage(page);
+//     const favorites = new Favorites(page);
+     
+//     await homepage.goto();
+//     await homepage.passCookies();
+//     await homepage.provideDeliveryAddress('Hans-Sachs-Straße 4/b-c, Berlin, 12205');
+//     await page.waitForTimeout(3000);
+  
+//     await homepage.gotoLogin2();
+//     await page.waitForTimeout(2000);
+  
+//     //await loginpage.passCookies();
+//     //await page.waitForTimeout(1000);
+//     await loginpage.passAuth();
+//     await page.waitForTimeout(2000);
+  
+//     await homepage.gotoFavorites();
+//     await page.waitForTimeout(2000);
+  
+//     await favorites.clickHeart();
+//     await page.waitForTimeout(2000);
+
+//     await expect(favorites.allFavorites).toBeEmpty();
+//   });
 
 });
