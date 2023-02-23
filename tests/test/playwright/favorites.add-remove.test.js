6f7586b131b223a81c5a8c92feb4eae5f69a96b8
Seva Ostakhnovich <seva_ost@mail.ru>
2023-02-23 14:10:32 +0400

    adding final version to main

--- tests/test/playwright/favorites.add-remove.test.js
+++ tests/test/playwright/favorites.add-remove.test.js
@@ -5,13 +5,13 @@ import { Catalog } from './pageobject/page.catalog';
 import { Favorites } from './pageobject/page.favorites';
 import { Header } from './pageobject/page.header';
 
-test.describe.only('Testing Favorites functionality: hearts and specific section', async () => {
+test.describe('Testing Favorites functionality', async () => {
 
   test.beforeEach(async ({ page }) => {
 
     const homepage = new Homepage(page);
     const loginpage = new LoginPage(page);
-    await homepage.gotoProduction();
+    await homepage.goto();
     await homepage.passCookies();
     await homepage.provideDeliveryAddress('Hans-Sachs-Straße 4/b-c, Berlin, 12205');
     await homepage.clickOnAnmeldenButton();
@@ -24,14 +24,10 @@ test.describe.only('Testing Favorites functionality: hearts and specific section
     const loginpage = new LoginPage(page);
     const header = new Header(page);
     const favorites = new Favorites(page);
-    await page.waitForLoadState('networkidle');
-    await Promise.all([
-      page.waitForRequest('**/toggle.json'),
-      favorites.favoriteButton.click()
-    ])
+    await favorites.favoriteButton.click();
   });
 
-  test('Check Favourites heart-icon in Catalog', async ({ page }) => {
+  test('Checking heart icon in the catalog and finding it in the Favoriten section ', async ({ page }) => {
   
     const homepage = new Homepage(page);
     const catalog = new Catalog(page);
@@ -42,8 +38,9 @@ test.describe.only('Testing Favorites functionality: hearts and specific section
     await catalog.firstHeart.click();
     await header.profileDropButton.click();
     await header.profileFavoriteLink.click();
-    await page.waitForLoadState('networkidle');
-    const zumProduktButton = await favorites.zumProduktButton.isVisible();
+    await page.waitForSelector('a > button')
+
+    const zumProduktButton = favorites.zumProduktButton.isVisible();
     expect(zumProduktButton).toBeTruthy();
   })
 });
